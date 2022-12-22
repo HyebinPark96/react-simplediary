6a7c1e85d1a5b45411432dcd7387461a52d749f2
bini2718 <bini2718@stninfotech.com>
2022-12-22 15:07:34 +0900

    리스트 조회 및 일기 데이터 추가, 수정, 삭제 구현

---
+++ src/Lifecycle2.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+const UnmountTest = () => {
+
+    useEffect(() => {
+        console.log('Mount!')
+        
+        return () => {
+            // UnMount 시점에 실행되게 됨
+            console.log('Unmount!');
+        }
+    }, [])
+    
+    return <div>Unmout Testing Component</div>
+}
+
+const Lifecycle = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    const toggle = () => {
+        setIsVisible(!isVisible);
+    }
+
+    return (
+        <div style={{ padding: 20}}>
+            <button onClick={toggle}>ON/OFF</button>
+            {/* 단락회로평가 */}
+            {isVisible && <UnmountTest />}
+        </div>
+    );
+}
+
+export default Lifecycle;
